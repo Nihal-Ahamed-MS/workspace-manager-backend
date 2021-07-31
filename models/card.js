7aff5e0a61b0fb0ff800da329a97eaaf747c05b4
LUCIFER-dev-king <nihaljesiya@gmail.com>
2021-07-31 23:07:25 +0530

    refracted code

--- models/card.js
+++ models/card.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-var cardShema = Schema({
-  cardName: {
-    type: String,
-    trim: true,
-    required: true,
+var cardShema = Schema(
+  {
+    cardName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    cardDesc: {
+      type: String,
+      trim: true,
+    },
   },
-  cardDesc: {
-    type: String,
-    trim: true,
-  },
-});
+  { timestamp: true }
+);
 
 module.exports = cardShema;
